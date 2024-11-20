02a1eeb83b4485c1836dde3710addd58e8cfb717
karam1215 <karam.haffar.2002@hotmail.com>
2024-11-20 15:36:14 +0300

    WIP: Save changes before switching to develop

--- backend/src/main/resources/database/user.sql
+++ backend/src/main/resources/database/user.sql
@@ -1,11 +1,12 @@
 CREATE TABLE users (
-    userID SERIAL,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    phone_number VARCHAR(18),
-    date_of_birth DATE,
-    password VARCHAR(255) NOT NULL,
-    currency VARCHAR(10) DEFAULT 'USD',
-    PRIMARY KEY(userID)
+                       userID SERIAL,
+                       name VARCHAR(100) NOT NULL,
+                       email VARCHAR(100) NOT NULL UNIQUE,
+                       phone_number VARCHAR(18),
+                       date_of_birth DATE,
+                       password VARCHAR(255) NOT NULL,
+                       currency VARCHAR(10) DEFAULT 'USD',
+                       PRIMARY KEY(userID)
 );
+SELECT * FROM users;
 
