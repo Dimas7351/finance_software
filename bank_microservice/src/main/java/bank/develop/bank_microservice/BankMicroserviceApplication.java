f290d220be8e47a59e87f8478911494789aa7ae0
karam1215 <karam.haffar.2002@hotmail.com>
2024-12-02 18:28:48 +0300

    microservice

---
+++ bank_microservice/src/main/java/bank/develop/bank_microservice/BankMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package bank.develop.bank_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankMicroserviceApplication.class, args);
+	}
+
+}
