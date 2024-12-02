fcf4cfa8432c306394f4b8df7e21347a3980cca4
karam1215 <karam.haffar.2002@hotmail.com>
2024-12-02 23:29:49 +0300

    findById

---
+++ backend/src/main/java/rpo/finance/software/mappers/AccountInfoMapper.java
@@ -0,0 +1,21 @@
+package rpo.finance.software.mappers;
+
+import org.springframework.stereotype.Service;
+import rpo.finance.software.DTO.user.Account;
+import rpo.finance.software.entities.User;
+
+import java.util.function.Function;
+
+@Service
+public class AccountInfoMapper implements Function<User, Account> {
+
+    @Override
+    public Account apply(User user) {
+        return new Account(user.getUserID(),
+                user.getName(),
+                user.getPhoneNumber(),
+                user.getBirthday(),
+                user.getEmail()
+        );
+    }
+}
---
+++ backend/src/main/java/rpo/finance/software/services/user/UserService.java
@@ -0,0 +1,30 @@
+package rpo.finance.software.services.user;
+
+import org.springframework.stereotype.Service;
+import rpo.finance.software.DTO.user.Account;
+import rpo.finance.software.entities.User;
+import rpo.finance.software.exceptions.AccountNotFoundException;
+import rpo.finance.software.mappers.AccountInfoMapper;
+import rpo.finance.software.repositories.UserRepository;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final AccountInfoMapper accountInfoMapper;
+
+    public UserService(UserRepository userRepository, AccountInfoMapper accountInfoMapper) {
+        this.userRepository = userRepository;
+        this.accountInfoMapper = accountInfoMapper;
+    }
+
+    public Account getUserAccount(Long id) {
+        return userRepository.findUserByUserID(id)
+                .map(accountInfoMapper)
+                .orElseThrow(() -> new AccountNotFoundException("Invalid"));
+    }
+
+    public void deleteAccount(long id) {
+        User user = userRepository.findUserByUserID(id).orElseThrow(() -> new AccountNotFoundException("Account not found!"));
+        userRepository.deleteById(id);
+    }
+}
