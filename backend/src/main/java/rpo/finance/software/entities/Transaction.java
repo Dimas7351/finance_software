e2b52ecc7fccc76310a4afcffa4128cbd67eddf5
4l3x4ndr10 <entakibora66@gmail.com>
2024-12-06 21:30:13 +0300

    Adapter

--- backend/src/main/java/rpo/finance/software/entities/Transaction.java
+++ backend/src/main/java/rpo/finance/software/entities/Transaction.java
@@ -30,7 +30,7 @@ public class Transaction {
     private User userID;
 
     @JoinColumn(name = "category_id")
-    private String categoryId;
+    private Integer categoryId;
 
     @Column(name = "amount", precision = 12)
     private Double amount;
