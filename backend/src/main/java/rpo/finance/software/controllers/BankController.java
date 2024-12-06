edba654537a82b3b76bbbaae113c05484b99efa5
Dimas735 <dmamugin@gmail.com>
2024-12-06 23:29:30 +0300

    Добавил сваггер для BankController

--- backend/src/main/java/rpo/finance/software/controllers/BankController.java
+++ backend/src/main/java/rpo/finance/software/controllers/BankController.java
@@ -1,5 +1,8 @@
 package rpo.finance.software.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +20,17 @@ public class BankController {
     private final BankAdapter bankAdapter;
 
     @GetMapping("/{userId}")
+    @Operation(summary = "Получение всех транзакций по userId")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Данные успешно вернулись")})
     public ResponseEntity<List<Transaction>> getTransactionsById(@PathVariable Long userId) {
         return bankAdapter.getTransactions(userId);
     }
 
     @PostMapping
+    @Operation(summary = "Получение всех транзакций по userId с указанием количества месяцев")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Данные успешно вернулись")})
     public ResponseEntity<List<Transaction>> getTransactionsByIdWithDate(@RequestBody DateTransactionDTO dateTransactionDto) {
         return bankAdapter.getTransactionsWithDate(dateTransactionDto);
     }
